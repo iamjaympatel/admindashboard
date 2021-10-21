093bf4e5072bdffffcf2c1aaa47755a38be03cea
Jay Patel <77323051+iamjaympatel@users.noreply.github.com>
2021-10-21 21:45:12 +0530

    remove link

--- client/src/components/dashboard/Footer.js
+++ client/src/components/dashboard/Footer.js
@@ -7,22 +7,7 @@ function CustomFooter() {
   return (
     <div>
       <Footer style={{ textAlign: 'center' }}>
-        Built with ❤️ by{' '}
-        <a
-          href="https://onasanyatunde.codes"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Onasanya Babatunde
-        </a>
-        |
-        <a
-          href="https://github.com/rammyblog/MERN-Admin-Dashboard"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Github Repo
-        </a>
+    
       </Footer>
     </div>
   );
